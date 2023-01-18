8c065cd0cae024440c84972007908dc925066d40
Maria Theresa de Sousa Ferreira <mariatheresa98@hotmail.com>
2023-01-18 16:21:45 -0300

    criacao do método equals em Médico

--- src/hospital/Medico.java
+++ src/hospital/Medico.java
@@ -1,6 +1,7 @@
 package hospital;
 
 import java.util.List;
+import java.util.Objects;
 
 public abstract class Medico extends Funcionario {
 	
@@ -44,4 +45,17 @@ public abstract class Medico extends Funcionario {
 	void iniciarAtendimento() {
 		System.out.println("Olá! O que você está sentindo?");
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Medico other = (Medico) obj;
+		return Objects.equals(CRM, other.CRM);
+	}
+		
 }
